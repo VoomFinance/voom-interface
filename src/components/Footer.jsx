bb4f2860e7419a50f547f69f83b356239c80b9a0
Voom Finance <dev@voom.finance>
2021-02-11 07:45:43 -0600

    add module lang

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,33 @@ import { useTranslation } from "react-i18next";
 import { tg, medium, github, explorerAddress, voom } from '../config/configs'
 
 const Footer = () => {
-    const { t } = useTranslation()
+    const { t, i18n } = useTranslation()
+
+    const getLang = () => {
+        const lang = localStorage.getItem('lang')
+        if (lang !== 'undefined' && lang !== null) {
+            return lang
+        } else {
+            return 'en'
+        }
+    }
+
+    const change = (e) => {
+        e.preventDefault()
+        let lang = getLang()
+        if(lang === 'en'){
+            localStorage.setItem('lang', 'es')
+            i18n.changeLanguage('es')
+        } else if(lang === 'es'){
+            localStorage.setItem('lang', 'en')
+            i18n.changeLanguage('en')
+        }        
+    }
 
     return (
         <div className="footer_container">
             <div className="footer_center">
-                <Nav className="justify-content-center footer" activeKey="/home">
+                <Nav className="justify-content-center footer">
                     <Nav.Item>
                         <Nav.Link href={github} target="_blank">Github</Nav.Link>
                     </Nav.Item>
@@ -23,7 +44,10 @@ const Footer = () => {
                         <Nav.Link href={medium} target="_blank">Medium</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link href="https://docs.binance.org/smart-chain/wallet/metamask.html" target="_blank">{t("Connect Metamask with Binance")}</Nav.Link>
+                        <Nav.Link href="https://academy.binance.com/en/articles/connecting-metamask-to-binance-smart-chain" target="_blank">{t("Connect Metamask with Binance")}</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link onClick={(e) => change(e)}>{t("Change language")}</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </div>
