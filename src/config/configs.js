26618cf38be14566fa37035d13fff59bca3cdc23
Voom Finance <dev@voom.finance>
2021-02-12 17:12:22 -0600

    add contract voom

--- src/config/configs.js
+++ src/config/configs.js
@@ -2,14 +2,14 @@
 
 export const idNetworkBSC = '0x38'
 export const idNetworkMetamask = '0x38'
-export const rcp = "http://127.0.0.1:8545"
-//export const rcp = "https://bsc-dataseed.binance.org/"
+//export const rcp = "http://127.0.0.1:8545"
+export const rcp = "https://bsc-dataseed.binance.org/"
 export const explorerHash = "https://bscscan.com/tx/"
 export const explorerAddress = "https://bscscan.com/address/"
 export const exchangeSwap = "https://exchange.pancakeswap.finance/"
 
-export const usdt = "0x66e62Dfd1D6b9eEb8614479c6E852F3BeCfD0386"
-export const voom = "0x260E450fB08fd3Bc580d050dd70D4e5E4b3339C2"
+export const usdt = "0x55d398326f99059fF775485246999027B3197955"
+export const voom = "0x9FF872140C94e3D6051C01eA0255Ea9bfE067E37"
 export const multicallAddr = "0x1ee38d535d541c55c9dae27b12edf090c608e6fb"
 export const chef = "0xf1ABC4a5fBC9637Ac783B21A7242687faa34aD7D"
 
